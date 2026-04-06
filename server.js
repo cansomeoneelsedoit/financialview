2f66b65bf10a2d1b1394a558bbe7af65f24dad1f
cansomeoneelsedoit <138751022+cansomeoneelsedoit@users.noreply.github.com>
2026-04-06 14:50:37 +0930

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname)));
 
 // Serve the main HTML file for all routes
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'financial-view-v3.3.html'));
+    res.sendFile(path.join(__dirname, 'financial-view-v3.4.html'));
 });
 
 app.listen(PORT, () => {
